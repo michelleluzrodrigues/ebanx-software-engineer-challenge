5b9d318a682f3a86df644e8a4acc06721ef1c268
michelleluzrodrigues <michellerodrigues268@gmail.com>
2025-08-07 23:38:27 -0300

    chore: adiciona classe principal para iniciar a aplicação Spring Boot

---
+++ src/main/java/com/ebax/challenge/EbanxChallengeApplication.java
@@ -0,0 +1,11 @@
+package com.ebax.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxChallengeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EbanxChallengeApplication.class, args);
+    }
+}
